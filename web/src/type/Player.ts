ed9aa39f1f3419f7ab32f90f67f795453f67e22f
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2023-07-20 21:36:21 +0530

    frontend setup

---
+++ web/src/type/Player.ts
@@ -0,0 +1,11 @@
+export interface Player {
+    socketId: string;
+    username: string;
+    isReady: boolean;
+    isFinished: boolean;
+    finishTime: number;
+    carIndex: number;
+
+    currentTextPosition: number;
+    typingSpeed: number;
+}
